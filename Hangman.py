fcc0a4f2097b11ad443eefda3fa2f3f24c677473
Armando Russo <46937969+armandorusso@users.noreply.github.com>
2019-05-24 00:37:42 -0400

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -58,9 +58,47 @@ o      |
        |
     ___|_____ 
     ''']
-words = ['ant', 'rat', 'owl', 'man', 'woman', 'deer', 'antelope', 'friend']
+words = ['ant', 'rat', 'owl', 'man', 'woman', 'deer', 'antelope', 'friend', 'python', 'snake', 'human', 'marvel', 'steve', 'tony']
 guessLetter = []
 guess = 0
+
+
+def midgame(guess, secretword, letter, blankword):
+    while guess != len(hangman):
+        if letter in secretword:
+            if letter in guessLetter:
+                print('You already guessed that letter! Enter a new guess: ')
+                newLetter = input()
+                midgame(guess, secretword, newLetter, blankword)
+            print('Nice, you got yourself a correct guess!')
+            drawboard(guess, secretword, letter, blankword)
+            print('Enter your next guess: ')
+            newLetter = input()
+            midgame(guess, secretword, newLetter, blankword)
+        else:
+            if letter in guessLetter:
+                print('You guessed that letter already! Enter a new letter: ')
+                newerLetter = input()
+                midgame(guess, secretword, newerLetter, blankword)
+            print('Incorrect guess!')
+            guess = guess + 1
+            drawboard(guess, secretword, letter, blankword)
+            print('You dont have many tries yet! Enter another word: ')
+            newerLetter = input()
+            midgame(guess, secretword, newerLetter, blankword)
+
+
+def startgame():
+    guess = 0
+    print('WELCOME TO HANGMAN!!!!')
+    print('We will try to guess a word for you and you will have to guess it!')
+    print('We will attempt to guess it now...please wait')
+    secretword = words[random.randint(0, len(words)) - 1]
+    time.sleep(2)
+    print('We got one! Now, what is your first guess?')
+    blankword = ['_'] * len(secretword)
+    letter = input()
+    midgame(guess, secretword, letter, blankword)
 
 
 def drawboard(guess, secretword, letter, blankword):
@@ -75,10 +113,11 @@ def drawboard(guess, secretword, letter, blankword):
     print(blankword)
     print('Your letter guesses so far have been: ')
     print(guessLetter)
-    if guess == len(guessLetter):
+    if guess == len(hangman) - 1:
         print('You lose! Play again? Yes: 1   No: 2')
         inp = input()
-        if inp == 1:
+        if int(inp) == 1:
+            guessLetter.clear()
             startgame()
         else:
             exit()
@@ -86,45 +125,14 @@ def drawboard(guess, secretword, letter, blankword):
         print('You guessed the word correctly! The word: ' + secretword)
         print('Play again? Yes: 1   No: 2')
         inp = input()
-        if inp == 1:
+        if int(inp) == 1:
+            guessLetter.clear()
             startgame()
         else:
             exit()
 
-def midgame(guess, secretword, letter, blankword):
-    while guess != len(hangman):
-        if letter in secretword:
-            if newLetter in guessLetter:
-                print('You already guessed that letter! Enter a new guess: ')
-                newLetter = input()
-                midgame(guess, secretword, newLetter, blankword)
-            print('Nice, you got yourself a correct guess!')
-            drawboard(guess, secretword, letter, blankword)
-            print('Enter your next guess: ')
-            newLetter = input()
-        else:
-            if newLetter in guessLetter:
-                print('You guessed that letter already! Enter a new letter: ')
-                newerLetter = input()
-                midgame(guess, secretword, newerLetter, blankword)
-            print('Incorrect guess!')
-            guess = guess + 1
-            drawboard(guess, secretword, letter, blankword)
-            print('You dont have many tries yet! Enter another word: ')
-            newerLetter = input()
-            midgame(guess, secretword, newerLetter, blankword)
 
 
-def startgame():
-    print('WELCOME TO HANGMAN!!!!')
-    print('We will try to guess a word for you and you will have to guess it!')
-    print('We will attempt to guess it now...please wait')
-    secretword = words[random.randint(0, len(words))]
-    time.sleep(2)
-    print('We got one! Now, what is your first guess?')
-    blankword = ['_'] * len(secretword)
-    letter = input()
-    midgame(guess, secretword, letter, blankword)
 
 
 startgame()
